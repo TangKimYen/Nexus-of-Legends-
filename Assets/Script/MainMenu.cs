0e33c207ecd48e96db226a6d6a94d27d236e1c54
Quynhdtn <136788239+dtnquin02@users.noreply.github.com>
2024-05-16 16:42:23 +0700

    TitlieScreen

---
+++ Assets/Script/MainMenu.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void PlayGame()
+    {
+
+    }
+    public void Tutorial()
+    {
+
+    }
+    public void ViewCredit() { }
+    public void Storyline() { }
+    public void QuitGame() { }
+    public void Login() { }
+    public void Register() { }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
