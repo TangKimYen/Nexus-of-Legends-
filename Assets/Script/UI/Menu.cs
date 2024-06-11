feb096807688bf14c724effbd62c9e2358ecc0b9
nthdattt <datnt02.work@gmail.com>
2024-06-11 13:46:44 +0700

    Party Lobby

--- Assets/Script/UI/Menu.cs
+++ Assets/Script/UI/Menu.cs
@@ -5,6 +5,20 @@ using UnityEngine.SceneManagement;
 
 public class Menu : MonoBehaviour
 {
+    public string menuName;
+    public bool open;
+    public void Open()
+    {
+        open = true;
+        gameObject.SetActive(true);
+    }
+
+    public void Close()
+    {
+        open = false;
+        gameObject.SetActive(false);
+    }
+
     public void PartyButton()
     {
         SceneManager.LoadScene("Partylobby");
