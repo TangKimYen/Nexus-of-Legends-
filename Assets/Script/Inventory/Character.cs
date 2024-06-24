082bd23d718eb01963a6e171b007e590232f6c81
PhongVy <phongvy.work@gmail.com>
2024-06-24 16:21:34 +0700

    Inventory

---
+++ Assets/Script/Inventory/Character.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character : MonoBehaviour
+{
+    public CharacterStat Strength;
+    public CharacterStat Intellect;
+    public CharacterStat Defense;
+    public CharacterStat Blood;
+    public CharacterStat Movement;
+    public CharacterStat AttackSpeed;
+
+    [SerializeField] Inventory inventory;
+    [SerializeField] EquipmentPanel equipmentPanel;
+
+    private void Awake()
+    {
+        inventory.OnItemRightClickedEvent += EquipFromInventory;
+        equipmentPanel.OnItemRightClickedEvent += UnequipFromEquipPanel;
+
+    }
+
+    private void EquipFromInventory(Item item)
+    {
+        Debug.Log("EquipFromInventory được gọi với item: " + item);
+        if (item is EquippableItem)
+        {
+            Equip((EquippableItem)item);
+        }
+    }
+
+    private void UnequipFromEquipPanel(Item item)
+    {
+        if (item is EquippableItem)
+        {
+            Unequip((EquippableItem)item);
+        }
+    }
+
+    public void Equip(EquippableItem item)
+    {
+        Debug.Log("Equip được gọi với item: " + item);
+        if (inventory.RemoveItem(item))
+        {
+            Debug.Log("Item đã được gỡ khỏi inventory");
+            EquippableItem previousItem;
+            if (equipmentPanel.AddItem(item, out previousItem))
+            {
+                Debug.Log("Item đã được thêm vào equipment panel");
+                if (previousItem != null)
+                {
+                    Debug.Log("Previous item: " + previousItem);
+                    inventory.AddItem(previousItem);
+                }
+            }
+            else
+            {
+                Debug.Log("Không thể thêm item vào equipment panel");
+                inventory.AddItem(item);
+            }
+        }
+    }
+
+    public void Unequip(EquippableItem item)
+    {
+        if (!inventory.IsFull() && equipmentPanel.RemoveItem(item))
+        {
+            inventory.AddItem(item);
+        }
+    }
+
+}
