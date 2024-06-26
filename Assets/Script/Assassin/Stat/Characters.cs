dc5e1c5927af9e014e1915bd973cb57f3a43d395
nthdattt <datnt02.work@gmail.com>
2024-06-26 18:45:39 +0700

    Fix Bug

--- Assets/Script/Assassin/Stat/Characters.cs
+++ Assets/Script/Assassin/Stat/Characters.cs
@@ -3,28 +3,28 @@ using System.Collections.Generic;
 using UnityEngine;
 using NOL.CharacterStats;
 
-public class Character
+public class Characters
 {
     public CharacterStat Strength;
 }
 
-public class Item
+public class Items
 {
     public string Name;
 
-    public void Equip(Character character)
+    public void Equip(Characters character)
     {
         character.Strength.AddModifier(new StatModifier(10, StatModType.Flat, this));
         character.Strength.AddModifier(new StatModifier(0.1f, StatModType.PercentAdd, this));
 
         foreach (var mod in character.Strength.StatModifiers)
         {
-            Item item = mod.Source as Item;
+            Items item = mod.Source as Items;
             Debug.Log(item.Name + ": " + mod.Value);
         }
     }
 
-    public void Unequip(Character character)
+    public void Unequip(Characters character)
     {
         character.Strength.RemoveAllModifiersFromSource(this);
     }
