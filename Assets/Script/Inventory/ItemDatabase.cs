ac5fae08e49fb9350670191670981e0bbcf20ac9
PhongVy <phongvy.work@gmail.com>
2024-07-22 23:09:05 +0700

    inventory

---
+++ Assets/Script/Inventory/ItemDatabase.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "ItemDatabase", menuName = "Inventory/ItemDatabase")]
+public class ItemDatabase : ScriptableObject
+{
+    [SerializeField] private List<Item> items = new List<Item>(); // Đảm bảo danh sách không rỗng
+
+    public Item GetItemById(string id)
+    {
+        foreach (var item in items)
+        {
+            if (item != null && item.itemId == id)
+            {
+                return item;
+            }
+        }
+        Debug.LogWarning($"No item found for ID: {id}");
+        return null;
+    }
+}
