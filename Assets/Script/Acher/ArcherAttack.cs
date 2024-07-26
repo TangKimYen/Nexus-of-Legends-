3c905ce6dd39ce668d8596bdb9e5eb7f0561b05b
DTNQuin <nhuquynhvliem@gmail.com>
2024-07-27 00:35:55 +0700

    Sound4Character

--- Assets/Script/Acher/ArcherAttack.cs
+++ Assets/Script/Acher/ArcherAttack.cs
@@ -26,7 +26,10 @@ public class ArcherAttack : MonoBehaviour
     private float cooldownSkill1 = Mathf.Infinity;
     private float cooldownSkill2 = Mathf.Infinity;
     private float cooldownSkill3 = Mathf.Infinity;
-    //[SerializeField] private AudioSource attackSoundEffect;
+    [SerializeField] private AudioSource attackSound;
+    [SerializeField] private AudioSource skill1Sound;
+    [SerializeField] private AudioSource skill2Sound;
+    [SerializeField] private AudioSource skill3Sound;
 
     private void Awake()
     {
@@ -42,18 +45,22 @@ public class ArcherAttack : MonoBehaviour
             dirX = Input.GetAxisRaw("Horizontal");
             if (Input.GetMouseButton(0) && cooldownAttack > attackCooldown && playerMovement.canAttack())
             {
+                attackSound.Play();
                 Attack();
             }
             else if (Input.GetButtonDown("Fire1") && cooldownSkill1 > skill1Cooldown && playerMovement.canAttack())
             {
+                skill1Sound.Play();
                 Skill1();
             }
             else if (Input.GetButtonDown("Fire2") && cooldownSkill2 > skill2Cooldown && playerMovement.canAttack())
             {
+                skill2Sound.Play();
                 Skill2();
             }
             else if (Input.GetButtonDown("Fire3") && cooldownSkill3 > skill3Cooldown && playerMovement.canAttack())
             {
+                skill3Sound.Play();
                 Skill3();
             }
             cooldownAttack += Time.deltaTime;
---
+++ Assets/Script/Authen/CheckAuthen.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckAuthen : MonoBehaviour
+{
+    public GameObject loggedInUI; // UI hiển thị khi người dùng đã đăng nhập
+    public GameObject notLoggedInUI; // UI hiển thị khi người dùng chưa đăng nhập
+
+    void Start()
+    {
+        // Kiểm tra trạng thái đăng nhập và cập nhật UI tương ứng
+        UpdateUIBasedOnLoginStatus();
+    }
+
+    private void UpdateUIBasedOnLoginStatus()
+    {
+        if (PlayerData.instance.isLoggedIn)
+        {
+            ShowLoggedInUI();
+        }
+        else
+        {
+            ShowNotLoggedInUI();
+        }
+    }
+
+    private void ShowLoggedInUI()
+    {
+        if (loggedInUI != null)
+        {
+            loggedInUI.SetActive(true);
+        }
+        if (notLoggedInUI != null)
+        {
+            notLoggedInUI.SetActive(false);
+        }
+    }
+
+    private void ShowNotLoggedInUI()
+    {
+        if (loggedInUI != null)
+        {
+            loggedInUI.SetActive(false);
+        }
+        if (notLoggedInUI != null)
+        {
+            notLoggedInUI.SetActive(true);
+        }
+    }
+}
